df81ae2c6d315002931e77c4f9124119150b9fcd
yancarlo0202 <139280488+yancarlo0202@users.noreply.github.com>
2024-08-15 14:29:40 -0400

    Initial commit

---
+++ src/main/java/me/dio/GftJavaIaApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftJavaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftJavaIaApplication.class, args);
+	}
+
+}
